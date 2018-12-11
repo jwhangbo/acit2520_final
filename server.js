bf9312925e096d8edb0bb643bccc94ff80ad7c27
John Whangbo <whangbok@hotmail.com>
2018-12-10 22:01:41 -0800

    Testing updates!

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const hbs = require('hbs');
 const fs = require('fs');
 
 var app = express();
+var port = process.env.PORT || 8080;
 
 hbs.registerPartials(__dirname + '/views/partials');
 
@@ -54,6 +55,6 @@ app.get('/404', (request, response) => {
 	})
 })
 
-app.listen(8080, () => {
-	console.log('Server is up on the port 8080');
+app.listen(port, () => {
+	console.log(`Server is up on the port ${port}`);
 });
